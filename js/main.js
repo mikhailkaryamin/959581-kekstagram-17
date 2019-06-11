7ad4de5ba53a84a93b95e64c7e6c0db17260faf3
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-06-11 23:39:22 +0500

    Линтер

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 var AVATARS = [
-  "img/avatar-1.svg", "img/avatar-2.svg", "img/avatar-3.svg", "img/avatar-4.svg", "img/avatar-5.svg", "img/avatar-6.svg"
+  'img/avatar-1.svg', 'img/avatar-2.svg', 'img/avatar-3.svg', 'img/avatar-4.svg', 'img/avatar-5.svg', 'img/avatar-6.svg'
 ];
 
 var descriptionTemplate = document.querySelector('#picture')
@@ -23,7 +23,7 @@ var maxURL = 25;
 
 // Получаем случайное число лайков
 
-var generateLikes = function (maxLikes, minLikes) {
+var generateLikes = function () {
   var likes = [];
 
   for (var i = 0; i < maxURL; i++) {
@@ -41,15 +41,15 @@ var getRandomArrayIndex = function (array) {
 
 // Получаем адрес картинки
 
-var generateLinksImgs = function (minURL, maxURL) {
+var generateLinksImgs = function () {
   var linkImg = [];
   for (var i = minURL; i <= maxURL; i++) {
     linkImg.push('photos/' + i + '.jpg');
-  };
+  }
 
   function compareRandom() {
     return Math.random() - 0.5;
-  };
+  }
 
   return linkImg.sort(compareRandom);
 };
@@ -65,7 +65,7 @@ var generateCommentData = function (names, messages, avatars) {
     avatar: avatar,
     message: message,
     name: name
-  }
+  };
 };
 
 // Получаем список комментариев к фото
@@ -77,14 +77,14 @@ var generateComments = function () {
     var numberComments = (Math.floor(Math.random() * maxURL + minURL));
     for (var j = 0; j < numberComments; j++) {
       commetsPhoto.push(generateCommentData(NAMES, MESSAGES, AVATARS));
-    };
+    }
     commentsPhotosList.push(commetsPhoto);
-  };
+  }
 
   return commentsPhotosList;
 };
 
- // Собираем массив лайков
+// Собираем массив лайков
 
 var likesPhotos = generateLikes(maxLikes, minLikes);
 
@@ -98,21 +98,21 @@ var commentsPhotosList = generateComments();
 
 // Получаем список описаний фотографий
 
-var generateDescriptionPhotos = function (likesPhotos, urlsPhotos, comments) {
+var generateDescriptionPhotos = function () {
   var descriptionPhotos = [];
 
   for (var i = 0; i < maxURL; i++) {
     descriptionPhotos.push({
       url: urlsPhotos[i],
       likes: likesPhotos[i],
-      comments: comments[i]
+      comments: commentsPhotosList[i]
     });
   }
 
   return descriptionPhotos;
-}
+};
 
-// Создаем DOM элементы "Описание фотографий"
+// Создаем DOM элементы 'Описание фотографий'
 
 var renderDescription = function (descriptionPhotos) {
   var elementDescription = descriptionTemplate.cloneNode(true);
@@ -141,7 +141,7 @@ var generateDescriptionList = function (descriptionPhotos) {
 var descriptionPhotos = generateDescriptionPhotos(likesPhotos, urlsPhotos, commentsPhotosList);
 var descriptionPhotosList = generateDescriptionList(descriptionPhotos);
 
-var insertDescriptionList = function (descriptionPhotosList) {
+var insertDescriptionList = function () {
   return descriptionPhotosList;
 };
 
