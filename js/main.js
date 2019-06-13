11437be9658c042e99969902432cdb39272f3a57
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-06-13 23:54:58 +0500

    Исправил замечания

--- js/main.js
+++ js/main.js
@@ -12,113 +12,115 @@ var AVATARS = [
   'img/avatar-1.svg', 'img/avatar-2.svg', 'img/avatar-3.svg', 'img/avatar-4.svg', 'img/avatar-5.svg', 'img/avatar-6.svg'
 ];
 
-var descriptionTemplate = document.querySelector('#picture')
+// Получаем моки
+
+var getMockData = function () {
+
+  var MIN_URL = 1;
+  var MAX_URL = 25;
+
+  // Получаем случайное число лайков
+
+  var generateLikes = function () {
+    var MIN_LIKES = 15;
+    var MAX_LIKES = 200;
+
+    return Math.floor(Math.random() * (MAX_LIKES - MIN_LIKES) + MIN_LIKES);
+  };
+
+  // Получаем случайный элемент массива
+
+  var getRandomArrayIndex = function (array) {
+    var i = Math.floor(Math.random() * (array.length - 1));
+    return array[i];
+  };
+
+  // Получаем адрес картинки
+
+  var generateLinksImgs = function () {
+    var linkImg = [];
+    for (var i = MIN_URL; i <= MAX_URL; i++) {
+      linkImg.push('photos/' + i + '.jpg');
+    }
+
+    function compareRandom() {
+      return Math.random() - 0.5;
+    }
+
+    return linkImg.sort(compareRandom);
+  };
+
+  // Получаем данные комментария
+
+  var generateCommentData = function (names, messages, avatars) {
+    var avatar = getRandomArrayIndex(avatars);
+    var message = getRandomArrayIndex(messages);
+    var name = getRandomArrayIndex(names);
+    return {
+      avatar: avatar,
+      message: message,
+      name: name
+    };
+  };
+
+  // Получаем список комментариев к фото
+
+  var generateComments = function () {
+    var commentsPhotos = [];
+    for (var i = 0; i < MAX_URL; i++) {
+      var comments = [];
+      var numberComments = (Math.floor(Math.random() * (MAX_URL - MIN_URL) + MIN_URL));
+      for (var j = 0; j < numberComments; j++) {
+        comments.push(generateCommentData(NAMES, MESSAGES, AVATARS));
+      }
+      commentsPhotos.push(comments);
+    }
+
+    return commentsPhotos;
+  };
+
+  // Получаем список описаний фотографий
+
+  var generateDescriptionPhotos = function () {
+    var descriptionPhotos = [];
+
+    for (var i = 0; i < MAX_URL; i++) {
+      descriptionPhotos.push({
+        url: urlsPhotos[i],
+        likes: generateLikes(),
+        comments: commentsPhotosList[i]
+      });
+    }
+
+    return descriptionPhotos;
+  };
+
+
+  // Собираем массив фото
+
+  var urlsPhotos = generateLinksImgs();
+
+  // Собираем массив комментариев
+
+  var commentsPhotosList = generateComments();
+  var mockDataPhoto = generateDescriptionPhotos(urlsPhotos, commentsPhotosList);
+
+  return mockDataPhoto;
+};
+
+// Создаем DOM фрагмент 'Описание фотографии'
+
+var renderDescription = function (mockDataPhotos) {
+
+  var descriptionTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-var minLikes = 15;
-var maxLikes = 200;
-var minURL = 1;
-var maxURL = 25;
-
-// Получаем случайное число лайков
-
-var generateLikes = function () {
-  var likes = [];
-
-  for (var i = 0; i < maxURL; i++) {
-    likes.push(Math.floor(Math.random() * maxLikes + minLikes));
-  }
-
-  return likes;
-};
-
-// Получаем случайный элемент массива
-
-var getRandomArrayIndex = function (array) {
-  return Math.floor(Math.random() * (array.length - 1));
-};
-
-// Получаем адрес картинки
-
-var generateLinksImgs = function () {
-  var linkImg = [];
-  for (var i = minURL; i <= maxURL; i++) {
-    linkImg.push('photos/' + i + '.jpg');
-  }
-
-  function compareRandom() {
-    return Math.random() - 0.5;
-  }
-
-  return linkImg.sort(compareRandom);
-};
-
-// Получаем данные комментария
-
-var generateCommentData = function (names, messages, avatars) {
-  var avatar = avatars[getRandomArrayIndex(avatars)];
-  var message = messages[getRandomArrayIndex(messages)];
-  var name = names[getRandomArrayIndex(names)];
-
-  return {
-    avatar: avatar,
-    message: message,
-    name: name
-  };
-};
-
-// Получаем список комментариев к фото
-
-var generateComments = function () {
-  var commentsPhotosList = [];
-  for (var i = 0; i < maxURL; i++) {
-    var commetsPhoto = [];
-    var numberComments = (Math.floor(Math.random() * maxURL + minURL));
-    for (var j = 0; j < numberComments; j++) {
-      commetsPhoto.push(generateCommentData(NAMES, MESSAGES, AVATARS));
-    }
-    commentsPhotosList.push(commetsPhoto);
-  }
-
-  return commentsPhotosList;
-};
-
-// Собираем массив лайков
-
-var likesPhotos = generateLikes();
-
-// Собираем массив фото
-
-var urlsPhotos = generateLinksImgs();
-
-// Собираем массив комментариев
-
-var commentsPhotosList = generateComments();
-
-// Получаем список описаний фотографий
-
-var generateDescriptionPhotos = function () {
-  var descriptionPhotos = [];
-
-  for (var i = 0; i < maxURL; i++) {
-    descriptionPhotos.push({
-      url: urlsPhotos[i],
-      likes: likesPhotos[i],
-      comments: commentsPhotosList[i]
-    });
-  }
-
-  return descriptionPhotos;
-};
-
-// Создаем DOM элементы 'Описание фотографий'
-
-var renderDescription = function (descriptionPhotos) {
   var elementDescription = descriptionTemplate.cloneNode(true);
-  elementDescription.querySelector('.picture__img').src = descriptionPhotos.url;
-  elementDescription.querySelector('.picture__likes').textContent = descriptionPhotos.likes;
-  elementDescription.querySelector('.picture__comments').textContent = descriptionPhotos.comments.length;
+
+  elementDescription.querySelector('.picture__img').src = mockDataPhotos.url;
+  elementDescription.querySelector('.picture__likes').textContent = mockDataPhotos.likes;
+  elementDescription.querySelector('.picture__comments').textContent = mockDataPhotos.comments.length;
 
   return elementDescription;
 };
@@ -126,21 +128,19 @@ var renderDescription = function (descriptionPhotos) {
 // Вставляет фрагменты в DOM
 
 var generateDescriptionList = function (descriptionPhotos) {
-  var fragment = document.createDocumentFragment();
+
   var similarListElement = document.querySelector('.pictures');
 
   for (var i = 0; i < descriptionPhotos.length; i++) {
-    fragment.appendChild(renderDescription(descriptionPhotos[i]));
+    var descriptionPhoto = renderDescription(descriptionPhotos[i]);
+    similarListElement.appendChild(descriptionPhoto);
   }
 
-  similarListElement.appendChild(fragment);
-  return fragment;
-
+  return similarListElement;
 };
 
-var descriptionPhotos = generateDescriptionPhotos(likesPhotos, urlsPhotos, commentsPhotosList);
-var descriptionPhotosList = generateDescriptionList(descriptionPhotos);
-
+var mockData = getMockData();
+var descriptionPhotosList = generateDescriptionList(mockData);
 var insertDescriptionList = function () {
   return descriptionPhotosList;
 };
