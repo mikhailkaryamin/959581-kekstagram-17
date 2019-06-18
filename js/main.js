a924d03c929aa41797d3b5be329bdb5c68fb9087
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-06-18 20:57:10 +0500

    выполнил 4 задание(подробности)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 'use strict';
 // Получаем моки
-var descriptionTemplate = document.querySelector('#picture')
-.content
-.querySelector('.picture');
 
 var getMockData = function () {
   var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
@@ -110,9 +107,10 @@ var getMockData = function () {
 };
 
 // Создаем DOM фрагмент 'Описание фотографии'
-
 var renderDescription = function (mockDataPhotos) {
-
+  var descriptionTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
   var elementDescription = descriptionTemplate.cloneNode(true);
 
   elementDescription.querySelector('.picture__img').src = mockDataPhotos.url;
@@ -123,7 +121,6 @@ var renderDescription = function (mockDataPhotos) {
 };
 
 // Вставляет фрагменты в DOM
-
 var generateDescriptionList = function (descriptionPhotos) {
 
   var fragment = document.createDocumentFragment();
@@ -142,8 +139,162 @@ var insertDescriptionList = function (descriptionPhotosList) {
   return similarListElement;
 };
 
-var mockData = getMockData();
-var descriptionPhotosList = generateDescriptionList(mockData);
+// Выводим фото на дисплей
+var displayPhotos = function () {
+  var mockData = getMockData();
+  var descriptionPhotosList = generateDescriptionList(mockData);
+  insertDescriptionList(descriptionPhotosList);
+};
 
-insertDescriptionList(descriptionPhotosList);
+// Загрузка изображения и редактирование изображения
+var uploadAndEditionImage = function () {
+
+  var imgUpload = document.querySelector('.img-upload');
+  var uploadFile = document.getElementById('upload-file');
+  var formEdition = imgUpload.querySelector('.img-upload__overlay');
+  var editionClose = imgUpload.querySelector('.img-upload__cancel');
+  var effectsList = document.querySelector('.effects__list');
+
+  var onEditionEscPress = function (evt) {
+    var ESC_KEYCODE = 27;
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeFormEdition();
+    }
+  };
+
+  var resetFormEdition = function () {
+    var scaleControl = document.querySelector('.scale__control--value');
+    var levelLine = document.querySelector('.effect-level__line');
+    var levelPin = levelLine.querySelector('.effect-level__pin');
+    var levelDepth = levelLine.querySelector('.effect-level__depth');
+
+    levelPin.style.left = '100%';
+    levelDepth.style.width = '100%';
+    scaleControl.value = '100%';
+  };
+
+  var openFormEdition = function () {
+    formEdition.classList.remove('hidden');
+    resetFormEdition();
+    document.addEventListener('keydown', onEditionEscPress);
+  };
+
+  var closeFormEdition = function () {
+    formEdition.classList.add('hidden');
+    uploadFile.value = '';
+  };
+
+  uploadFile.addEventListener('change', function () {
+    openFormEdition();
+  });
+
+  editionClose.addEventListener('click', function () {
+    closeFormEdition();
+  });
+
+  // Накладываем эффекты на фото
+
+  effectsList.addEventListener('change', function (evt) {
+    var filterName = evt.target.value;
+
+    getEffectPreview(filterName);
+  });
+
+  var getEffectPreview = function (effect) {
+    var previewPhoto = imgUpload.querySelector('.img-upload__preview');
+    var currentFilterClassName = '';
+    var imgPreview = previewPhoto.querySelector('img');
+    var slider = document.querySelector('.img-upload__effect-level');
+
+    if (currentFilterClassName) {
+      imgPreview.classList.remove(currentFilterClassName);
+    }
+
+    if (effect !== 'none') {
+      slider.classList.remove('hidden');
+      currentFilterClassName = 'effects__preview--' + effect;
+      imgPreview.classList.add(currentFilterClassName);
+    } else {
+      slider.classList.add('hidden');
+      currentFilterClassName = '';
+    }
+  };
+};
+
+displayPhotos();
+uploadAndEditionImage();
+// var effectLevelPin = imgUpload.querySelector('.effect-level__pin');
+// var generateEffect = function () {
+//   var style = window.getComputedStyle(effectLevelPin);
+//   var left = parseInt((style.getPropertyValue('left')), 10);
+//   var obejctsEffects = {
+//     chrome: {
+//       minValue: 0,
+//       maxValue: 1
+//     },
+
+//     sepia: {
+//       minValue: 0,
+//       maxValue: 1
+//     },
+
+//     marvin: {
+//       minValue: 0,
+//       maxValue: 100
+//     },
+
+//     phobos: {
+//       minValue: 0,
+//       maxValue: 3
+//     },
+
+//     heat: {
+//       minValue: 1,
+//       maxValue: 3
+//     }
+//   };
+//   if (checkedElement !== 'none') {
+//     var maxValue = obejctsEffects[checkedElement].maxValue;
+//     var minValue = obejctsEffects[checkedElement].minValue;
+//     var range = maxValue - minValue;
+//     var effectValue = range * left / 100 + minValue;
+//   }
+//   return effectValue;
+// };
+
+// var searchChecked = function () {
+//   var effectsList = document.querySelector('.effects__list');
+//   var elementEffect = effectsList.querySelector('input[type=radio]:checked');
+//   var elementValue = elementEffect.value;
+//   return elementValue;
+// };
+
+// var checkedElement = searchChecked();
+// var valueEffect = generateEffect();
+
+// var getEffectString = function (effect, value) {
+//   var effectString;
+//   switch (effect) {
+//     case 'chrome':
+//       effectString = 'grayscale(' + value + ')';
+//       break;
+//     case 'sepia':
+//       effectString = 'sepia(' + value + ')';
+//       break;
+//     case 'marvin':
+//       effectString = 'invert(' + value + ')';
+//       break;
+//     case 'phobos':
+//       effectString = 'blur(' + value + 'px)';
+//       break;
+//     case 'heat':
+//       effectString = 'brightness(' + value + ')';
+//       break;
+//     default:
+//       effectString = '';
+//       break;
+//   }
+//   return effectString;
+// };
+
 
