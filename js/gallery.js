ca59b4a0f6fcbe4a1ea245e28df29704d885b0b4
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-06-28 23:18:56 +0500

    Исправил замечания

--- js/gallery.js
+++ js/gallery.js
@@ -29,12 +29,7 @@
 
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background: transparent;';
-    node.style.position = 'absolute';
-    node.style.top = '20px';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
+    node.classList.add('errorHadler');
     node.textContent = errorMessage;
     document.body.insertAdjacentElement('afterbegin', node);
   };
