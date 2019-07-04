eb26360dc1ca59c08cf04bba6f59d15f8e0f476a
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-07-04 23:42:45 +0500

    2nd

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -5,24 +5,35 @@
   var bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
   var picturesElement = document.querySelector('.pictures');
 
-  var displayBigPicture = function (dataPhotos) {
-    var comments = dataPhotos[0].comments;
+  // Создаем комментарий
+  var createComment = function (comment) {
+    var commentElement = bigPictureElement.querySelector('.social__comment');
+    var elementDescription = commentElement.cloneNode(true);
+
+    elementDescription.querySelector('.social__text').textContent = comment.message;
+    elementDescription.querySelector('.social__picture').src = comment.avatar;
+
+    return elementDescription;
+  };
+
+  // Сбрасывает список комментариев
+  var clearCommentsList = function () {
+    var comments = bigPictureElement.querySelectorAll('.social__comment');
+    comments.forEach(function (el) {
+      el.remove();
+    });
+  };
+
+  // Создаем и выводим фото с комментариями
+  var displayBigPicture = function (dataPhoto) {
+    var comments = dataPhoto.comments;
     var pictureImgElement = bigPictureElement.querySelector('.big-picture__img');
     var pictureImgSrcElement = pictureImgElement.querySelector('img');
     var likesCountElement = bigPictureElement.querySelector('.likes-count');
     var commentsCountElement = bigPictureElement.querySelector('.comments-count');
     var socialCaption = bigPictureElement.querySelector('.social__caption');
 
-    var createComment = function (comment) {
-      var commentElement = bigPictureElement.querySelector('.social__comment');
-      var elementDescription = commentElement.cloneNode(true);
-
-      elementDescription.querySelector('.social__text').textContent = comment.message;
-      elementDescription.querySelector('.social__picture').src = comment.avatar;
-
-      return elementDescription;
-    };
-
+    // Вставляем комментарии
     var insertCommentsList = function () {
       var fragment = document.createDocumentFragment();
       var similarListElement = document.querySelector('.social__comments');
@@ -30,12 +41,15 @@
       comments.forEach(function (comment) {
         fragment.appendChild(createComment(comment));
       });
+
+      clearCommentsList();
+
       similarListElement.appendChild(fragment);
 
-      pictureImgSrcElement.src = dataPhotos[0].url;
-      likesCountElement.textContent = dataPhotos[0].likes;
-      commentsCountElement.textContent = dataPhotos[0].comments.length;
-      socialCaption.textContent = dataPhotos[0].description;
+      pictureImgSrcElement.src = dataPhoto.url;
+      likesCountElement.textContent = dataPhoto.likes;
+      commentsCountElement.textContent = dataPhoto.comments.length;
+      socialCaption.textContent = dataPhoto.description;
     };
 
     insertCommentsList();
@@ -49,6 +63,7 @@
     bigPictureElement.classList.add('hidden');
   };
 
+  // Открытие формы
   var openBigPictureForm = function () {
     var commentCountElement = bigPictureElement.querySelector('.social__comment-count');
     var commentLoaderElement = bigPictureElement.querySelector('.comments-loader');
@@ -58,13 +73,27 @@
     commentLoaderElement.classList.add('hidden');
   };
 
+  var searchIndexPicture = function (evt, pictures) {
+    var currentPicture = evt.target.attributes.src.nodeValue;
+
+    // Ищем индекс фото
+    var pictureIndex = pictures.map(function (e) {
+      return e.url;
+    }).indexOf(currentPicture);
+
+    return pictureIndex;
+  };
+  // Обработчик события(открытие окна)
   var showBigPicture = function (data) {
-    // var smallPictureElement = picturesElement.querySelector('.picture');
 
     picturesElement.addEventListener('click', function (evt) {
+      var indexPicture = searchIndexPicture(evt, data);
+
       openBigPictureForm();
-      displayBigPicture(data);
-      console.log(evt);
+
+      displayBigPicture(data[indexPicture]);
+
+
       bigPictureCloseElement.addEventListener('click', function () {
         bigPictureElement.classList.add('hidden');
       });
