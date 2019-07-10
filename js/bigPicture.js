02f62f81ef1ea1ce9e8e7a8d3ec65a4db05f8712
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-07-10 21:30:57 +0500

    ol

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,7 +1,14 @@
 'use strict';
 
 (function () {
+  var COUNT_COMMENTS = 5;
+
   var bigPictureElement = document.querySelector('.big-picture');
+  var similarListElement = document.querySelector('.social__comments');
+  var commentLoaderElement = bigPictureElement.querySelector('.comments-loader');
+  var comments;
+  var counter = 0;
+  var startCounter = 0;
 
   // Создаем комментарий
   var createComment = function (comment) {
@@ -16,30 +23,58 @@
 
   // Сбрасывает список комментариев
   var clearCommentsList = function () {
-    var comments = bigPictureElement.querySelectorAll('.social__comment');
-    comments.forEach(function (el) {
+    var commentsElements = bigPictureElement.querySelectorAll('.social__comment');
+    commentsElements.forEach(function (el) {
       el.remove();
     });
   };
 
+  // Встваляет счетчик комментариев в разметку
+  var insertCountComments = function () {
+    var commentCountElement = bigPictureElement.querySelector('.social__comment-count');
+    var stringCountCommentElement = commentCountElement.innerHTML = counter + ' из <span class="comments-count">' + comments.length + '</span> комментариев';
+
+    return stringCountCommentElement;
+  };
+
+  // Получаем фрагмент массива комментарие и вызываем счетчик комментариев
+  var getFragmentCommentsList = function () {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = startCounter; i < COUNT_COMMENTS + startCounter; i++) {
+      if (counter === comments.length) {
+        commentLoaderElement.classList.add('hidden');
+        insertCountComments();
+        break;
+      } else {
+        fragment.appendChild(createComment(comments[i]));
+        counter++;
+
+        if (counter === comments.length) {
+          commentLoaderElement.classList.add('hidden');
+        }
+      }
+    }
+
+    insertCountComments();
+
+    startCounter += 5;
+    return fragment;
+  };
+
   // Создаем и выводим фото с комментариями
   var displayBigPicture = function (dataPhoto) {
-    var comments = dataPhoto.comments;
     var pictureImgElement = bigPictureElement.querySelector('.big-picture__img');
     var pictureImgSrcElement = pictureImgElement.querySelector('img');
     var likesCountElement = bigPictureElement.querySelector('.likes-count');
     var commentsCountElement = bigPictureElement.querySelector('.comments-count');
     var socialCaption = bigPictureElement.querySelector('.social__caption');
-    var fragment = document.createDocumentFragment();
-    var similarListElement = document.querySelector('.social__comments');
+    comments = dataPhoto.comments;
 
-    comments.forEach(function (comment) {
-      fragment.appendChild(createComment(comment));
-    });
-
+    var firstLoadCommentList = getFragmentCommentsList(comments);
     clearCommentsList();
 
-    similarListElement.appendChild(fragment);
+    similarListElement.appendChild(firstLoadCommentList);
 
     pictureImgSrcElement.src = dataPhoto.url;
     likesCountElement.textContent = dataPhoto.likes;
@@ -49,12 +84,7 @@
 
   // Открытие формы
   var openBigPictureForm = function () {
-    var commentCountElement = bigPictureElement.querySelector('.social__comment-count');
-    var commentLoaderElement = bigPictureElement.querySelector('.comments-loader');
-
     bigPictureElement.classList.remove('hidden');
-    commentCountElement.classList.add('hidden');
-    commentLoaderElement.classList.add('hidden');
   };
 
   // Закрытие по ESC
@@ -65,10 +95,20 @@
   // Обработчик закрытия
   var closeBigPictureForm = function () {
     bigPictureElement.classList.add('hidden');
+    commentLoaderElement.classList.remove('hidden');
 
+    counter = 0;
+    startCounter = 0;
+
+    commentLoaderElement.removeEventListener('click', onLoaderComments);
     document.removeEventListener('click', closeBigPictureForm);
   };
 
+  // Обработчик подгрузки комментариев
+  var onLoaderComments = function () {
+    var fragmentCommentList = getFragmentCommentsList(comments);
+    similarListElement.appendChild(fragmentCommentList);
+  };
   // Форма с ее содержимым
   var showBigPicture = function (data) {
     var bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
@@ -76,6 +116,7 @@
     displayBigPicture(data);
     openBigPictureForm();
 
+    commentLoaderElement.addEventListener('click', onLoaderComments);
     bigPictureCloseElement.addEventListener('click', closeBigPictureForm);
     document.addEventListener('keydown', onFormEscPress);
   };
